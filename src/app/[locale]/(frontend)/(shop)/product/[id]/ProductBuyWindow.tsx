5b4ba59feb2e0c8d5c46918bed0d7da6fb13a6f1
daniel petcov <danielpetcov.work@gmail.com>
2025-03-13 02:28:56 +0200

    very big update on product page

--- src/app/[locale]/(frontend)/(shop)/product/[id]/ProductBuyWindow.tsx
+++ src/app/[locale]/(frontend)/(shop)/product/[id]/ProductBuyWindow.tsx
@@ -2,7 +2,7 @@ import BuyButtons from "./BuyButtons";
 import { Product } from "@/payload-types";
 const ProductBuyWindow = ({ product }: { product: Product }) => {
     return (
-        <div className="sticky top-0 bg-white p-5 lg:p-10 rounded-md h-fit">
+        <div className="sticky top-5 bg-white p-5 lg:p-10 rounded-md h-fit">
             <div className="flex flex-col gap-3 h-fit">
                 <p className="text-slate-800 text-xl md:text-2xl font-semibold">{product.title}</p>
                 <div>
