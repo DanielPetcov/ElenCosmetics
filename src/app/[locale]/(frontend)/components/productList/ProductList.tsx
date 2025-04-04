6f101aaa5aa32006eb687f0a23ad8974365a5511
daniel petcov <danielpetcov.work@gmail.com>
2025-04-04 17:54:10 +0300

    added translation

--- src/app/[locale]/(frontend)/components/productList/ProductList.tsx
+++ src/app/[locale]/(frontend)/components/productList/ProductList.tsx
@@ -12,8 +12,10 @@ interface ProductListProps {
 import 'swiper/css';
 import 'swiper/css/free-mode';
 import ProductListSwiper from "./ProductListSwiper";
+import { useTranslations } from "next-intl";
 
 const ProductList: React.FC<ProductListProps> = ({ category, title, locale }) => {
+    const t = useTranslations('ProductList');
     if (typeof category == 'string') return null
     const products: Product[] = Array.isArray(category.products) && typeof category !== 'string' ? category.products.filter((product): product is Product => typeof product !== 'string') : [];
 
@@ -22,17 +24,17 @@ const ProductList: React.FC<ProductListProps> = ({ category, title, locale }) =>
             <div className="flex flex-col gap-5 lg:gap-10 max-w-full text-gray-700">
                 <div className="flex flex-col gap-3">
                     <div className="text-2xl md:text-3xl lg:text-4xl font-semibold text-center uppercase">{title}</div>
-                    <Link href={`/collection/${category.id}`} locale={locale} className="text-center text-base md:tex-xl lg:text-2xl hover:underline">Vezi mai multe</Link>
+                    <Link href={`/collection/${category.id}`} locale={locale} className="text-center text-base md:tex-xl lg:text-2xl hover:underline">{t('seeMore')}</Link>
                 </div>
                 {products.length > 0 ? (
-                    <Suspense fallback={<div>is loading</div>}>
+                    <Suspense fallback={<div>{t('loading')}</div>}>
                         <ProductListSwiper
                             locale={locale}
                             products={products}
                         />
                     </Suspense>
                 ) : (
-                    <div className="text-center text-gray-500">Momentan nu sunt produse disponibile</div>
+                    <div className="text-center text-gray-500">{t('noProducts')}</div>
                 )
                 }
             </div>
